4854acdf82137b80b20bcc4b88f687b432935121
xaviguasch <xaviguasch@gmail.com>
2019-05-08 13:21:28 +0200

    added disabled feature when there are no rolls left

--- src/Die.js
+++ src/Die.js
@@ -15,10 +15,10 @@ class Die extends Component {
   }
 
   render() {
-    const { numberWords, locked, val } = this.props
+    const { numberWords, locked, val, disabled } = this.props
     let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `
     if (locked) classes += 'Die-locked'
-    return <i className={classes} onClick={this.handleClick} />
+    return <i className={classes} onClick={this.handleClick} disabled={disabled} />
   }
 }
 
